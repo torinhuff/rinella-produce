2853b844c05d5244ca36fb4f6d293491725a8464
Torin Huff <torinhuff@yahoo.com>
2019-01-15 15:30:55 -0800

    cleaned up js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
 
-
-// window.onload=function() {
-//   // Month,Day,Year,Hour,Minute,Second
-//   upTime('jan,01,1914,00:00:00');
-// };
-
 function upTime(countTo) {
   now = new Date();
   countTo = new Date(countTo);
@@ -25,48 +19,12 @@ function upTime(countTo) {
   upTime.to=setTimeout(function(){ upTime(countTo); },1000);
 };
 
-$(".bars-cell").click(function(){
-  $(".navbar").slideToggle();
-  $(".dropdown").css("display", "none");
-
-});
-
 $("ul li").click(function(){
   $("ul ul").slideUp();
   $(this).find("ul").slideToggle();
 
 });
 
-$(window).resize(function(){
-  if($(window).width() > 768){
-    $("ul").removeAttr("style");
-  }
-
-});
-
 window.onload=function() {
-    // $("#hiring-object").slideUp(3300);
-    // $("#walk-in-object").slideUp(3600);
-    // $("#mailing-object").slideUp(3900);
-    // // $(".opening-reveal").show();
-    // $(".opening-slide-up").slideDown(4600);
-    // $("#hiring-object").slideDown();
-    // $("#walk-in-object").slideDown();
-    // $("#mailing-object").slideDown();
-    // $("#.opening-reveal").show().slideDown(4000);
-    // $(".opening-slide-up").show();
-
-    // Month,Day,Year,Hour,Minute,Second
     upTime('jan,01,1914,00:00:00');
   };
-
-
-  // window.onload=function() {
-  //   // Month,Day,Year,Hour,Minute,Second
-  //   upTime('jan,01,1914,00:00:00');
-  // };
-
-  document.getElementById('search').onsubmit = function() {
-        window.location = 'http://www.google.com/search?q=site:rinellaproduce.com ' + document.getElementById('textSearch').value;
-        return false;
-    }
